d5f26c874dc2b5304a45b7b994c4d3cf8cb47e18
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2024-07-06 21:38:54 +0300

    Program now saves exercise data to Google Sheets via Sheety API

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 import os
 import requests
+import datetime
 
 load_dotenv()
 APP_ID = os.getenv('APP_ID')
 API_KEY = os.getenv('API_KEY')
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheets_endpoint = f"https://api.sheety.co/{os.getenv('USERNAME')}/workoutTracker/workouts"
 
 header = {
     "x-app-id": APP_ID,
@@ -13,8 +15,21 @@ header = {
 }
 
 exercise_params = {
-    "query": input("What exercises did you perform? ")
+    "query": input("What exercises did you perform? \n")
 }
 
-response = requests.post(exercise_endpoint, headers=header, json=exercise_params)
-print(response.text)
+exercise_response = requests.post(exercise_endpoint, headers=header, json=exercise_params)
+exercise_data = exercise_response.json()["exercises"]
+
+for exercise in exercise_data:
+    sheets_params = {
+        "workout": {
+            "date": datetime.datetime.now().strftime("%d/%m/%Y"),
+            "time": datetime.datetime.now().strftime("%H:%M:%S"),
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+    sheets_response = requests.post(sheets_endpoint, json=sheets_params)
+    print(sheets_response.text)

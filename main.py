bf5274a2830902c0c51983e773a062c2a0d4b7f4
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2024-07-06 20:18:45 +0300

    Exercise API works

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 APP_ID = os.getenv('APP_ID')
 API_KEY = os.getenv('API_KEY')
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
+header = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+exercise_params = {
+    "query": input("What exercises did you perform? ")
+}
+
+response = requests.post(exercise_endpoint, headers=header, json=exercise_params)
+print(response.text)
